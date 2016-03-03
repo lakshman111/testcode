9132a259e495786394f28e269df60a1fdd194a1c
Lakshman Mody <lakshman111@gmail.com>
2016-03-02 23:19:27 -0600

    mergeSort doesnt work

--- testcode.py
+++ testcode.py
@@ -531,17 +531,60 @@
 # print(anagrams('abc'))
 
 # Fibonacci is more efficient as a loop than recursive!
-def loopfib(n):
-  curr = 1
-  prev = 1
-  for i in range(n-2):
-    curr,prev = curr+prev, curr
-  return curr
+# def loopfib(n):
+#   curr = 1
+#   prev = 1
+#   for i in range(n-2):
+#     curr,prev = curr+prev, curr
+#   return curr
 
-print(loopfib(6))
+# print(loopfib(6))
 
+# MERGESORT!!!!
+def merge(lst1, lst2, lst3):
+  # merge sorted lists lst1 and lst2 into lst3
+  i1,i2,i3=0,0,0 #allstartatthefront
+  # these indexes keep track of current position in each list
+  n1, n2 = len(lst1), len(lst2)
+    # Loop while both lst1 and lst2 have more items
+  while i1 < n1 and i2 < n2:
+    if lst1[i1] < lst2[i2]:  # top of lst1 is smaller
+      lst3[i3] = lst1[i1]  #  copy it into current spot in lst3
+      i1 = i1 + 1
+    else:                    # top of lst2 is smaller
+      lst3[i3] = lst2[i2]  #  copy it into current spot in lst3
+      i2 = i2 + 1
+    i3 = i3 + 1              # item added to lst3, update position
+  # Here either lst1 or lst2 is done. One of the following loops will
+  # execute to finish up the merge.
+  # Copy remaining items (if any) from lst1
+  while i1 < n1:
+    lst3[i3] = lst1[i1]
+    i1 = i1 + 1
+    i3 = i3 + 1
+  # Copy remaining items (if any) from lst2
+  while i2 < n2:
+    lst3[i3] = lst2[i2]
+    i2 = i2 + 1
+    i3 = i3 + 1
 
+  return lst3
 
+def mergeSort(nums):
+  # Put items of nums in ascending order
+  n = len(nums)
+  # Do nothing if nums contains 0 or 1 items
+  if n > 1:
+    # split into two sublists
+    m = n // 2
+    nums1, nums2 = nums[:m], nums[m:]
+    # recursively sort each piece
+    mergeSort(nums1)
+    mergeSort(nums2)
+    # merge the sorted pieces back into original list
+    return merge(nums1, nums2, nums)
+
+print(mergeSort([4,5,3,5,6,7,8,1]))
 
 
 
